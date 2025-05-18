125f85455d496b850e230129f64a8be7c753d0c6
gohyeoncheol <huncher00@naver.com>
2025-05-18 13:58:32 +0900

    fix handler

---
+++ cmd/server/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"github.com/RakunKo/Tistory-Readme-Go/internal/controller"
+	"log"
+	"net/http"
+)
+
+//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
+// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+
+func Handler() {
+	http.HandleFunc("/tistory-badge/", controller.BadgeHandler)
+	fmt.Println("Starting server on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
